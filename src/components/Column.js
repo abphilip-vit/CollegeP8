5b4d95947e2ed88c6d7bd39415dc1c529c50f316
Sanjay Babu <sanjaybabu2210@gmail.com>
2021-05-21 06:45:51 +0530

    Update Column.js

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,13 +1,11 @@
 const Column = ({ eventData }) => {
     const list = eventData.map( ev => {
-        if(ev.categories[0].id === 8){
             return <div>
             <ul>
-                <li>ID: <strong>{ ev.id }</strong></li>
-                <li>Date: <strong> {ev.geometries[0].date} </strong></li>
+                <li>Latitude: <strong>{ev[0] }</strong></li>
+                <li>Longitude: <strong> {ev[1]} </strong></li>
             </ul>
             </div>
-        }
     })
 
     return <div className="column-data">
