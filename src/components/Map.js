1fa3e71ecef82d5c78d00d61a7ad42afa4bfe0b8
Sanjay Babu <sanjaybabu2210@gmail.com>
2021-05-21 06:45:25 +0530

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,9 +8,7 @@ const Map = ({eventData, center, zoom}) => {
     const [locationInfo, setLocationInfo] = useState(null)
 
     const markers = eventData.map( ev => {
-        if(ev.categories[0].id === 8){
-            return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({id: ev.id, date: ev.geometries[0].date})}/>
-        }
+            return <LocationMarker lat={ev[0]} lng={ev[1]}/>
     })
     
     return (
@@ -29,10 +27,11 @@ const Map = ({eventData, center, zoom}) => {
 
 Map.defaultProps = {
     center: {
-        lat: 13.0827,
-        lng: 80.2707
+        lat: 40.676789,
+        lng: -73.921611
     },
     zoom: 15
+
 }
 
 export default Map;
