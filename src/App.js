5095d958f647a33c2d02db2e278e787602058bf7
Sanjay Babu <sanjaybabu2210@gmail.com>
2021-05-21 06:44:43 +0530

    final change

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import {useState, useEffect} from 'react';
 import Header from './components/Header'
 import Column from './components/Column'
 import axios from 'axios';
+import firebase from './Firebase';
 
 function App() {
 
   const [eventData, setEventData] = useState([]);
+  const [todoList, setTodoList] = useState([]);
 
   useEffect(() => {
     // const fetchEvents = async () => {
@@ -16,23 +18,37 @@ function App() {
     // }
     // fetchEvents()
     // console.log(eventData);
-    axios.defaults.baseURL = 'https://tarp-project-ee72f-default-rtdb.firebaseio.com/';
+//     axios.defaults.baseURL = 'https://tarp-project-ee72f-default-rtdb.firebaseio.com/';
 
-axios.get('https://tarp-project-ee72f-default-rtdb.firebaseio.com/coordinate.json').then(responses => {
-  let event = responses.data;
-  setEventData(event);
-  console.log(event);
-})
+// axios.get('https://tarp-project-ee72f-default-rtdb.firebaseio.com/coordinate.json').then(responses => {
+//   let event = responses.data;
+//   console.log(event);
+//   setEventData(event);
+//   console.log(eventData); 
+//   console.log(1);
 
-  }, []);
-
-
+  const todoRef = firebase.database().ref('coordinate');
+      todoRef.on('value', (snapshot) => {
+        const todos = snapshot.val();
+        console.log(todos);
+        const todoList = [];
+        for(let i = 1; i < todos.length; i++){
+        const cd = todos[i].split(",");
+        cd[0] = parseFloat(cd[0]);
+        cd[1] = parseFloat(cd[1]);
+        todoList.push(cd);
+      }
+        console.log(todoList);
+       setTodoList(todoList);
+      });
+      todoList.map((todo, i) => console.log(todo));
+    }, []);
 
   return (
     <div className="App">
       <Header />
-      {/* <Map eventData={eventData}/>
-      <Column eventData={eventData}/> */}
+      <Map eventData={todoList}/>
+      <Column eventData={todoList}/>
     </div>
   );
 }
